de25ea96c52dd25d7f4f5ddd5d06960d1cf7c290
Sandeep Konda <kondasandeep56@gmail.com>
2026-09-22 19:39:40 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    basicSsl(),
   ],
 
   server: {
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    https: true,
   },
 })
