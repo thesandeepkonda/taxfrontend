a98f56380368964466ad7e9de2bb0c181c16ddf1
Sandeep Konda <kondasandeep56@gmail.com>
2026-09-24 19:03:29 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
+
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { store, persistor } from './store';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/crm">
         <App />
-      </PersistGate>
-    </Provider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
